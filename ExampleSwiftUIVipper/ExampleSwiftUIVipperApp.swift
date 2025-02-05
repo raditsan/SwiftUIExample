b4273b884f9fc2f893448078d6d119ffcda63ffb
raditsan <raditsaditya2@gmail.com>
2025-02-05 08:09:50 +0700

    Initial Commit

---
+++ ExampleSwiftUIVipper/ExampleSwiftUIVipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleSwiftUIVipperApp.swift
+//  ExampleSwiftUIVipper
+//
+//  Created by raditsan on 05/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ExampleSwiftUIVipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExampleSwiftUIVipperTests/ExampleSwiftUIVipperTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleSwiftUIVipperTests.swift
+//  ExampleSwiftUIVipperTests
+//
+//  Created by raditsan on 05/02/25.
+//
+
+import Testing
+@testable import ExampleSwiftUIVipper
+
+struct ExampleSwiftUIVipperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
